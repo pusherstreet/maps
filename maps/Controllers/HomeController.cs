050ce6643395fd617ab946097e89fd615e5b4e77
pusherstreet <vitaliy.diachuk@gmail.com>
2017-11-29 13:37:12 +0200

    initial commit

---
+++ maps/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+
+namespace maps.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            string key = GetApiKey();
+            return View((object)key);
+        }
+        private string GetApiKey()
+        {
+            return System.IO.File.ReadAllText(Server.MapPath("~/key.txt"));
+        }
+
+    }
+}
